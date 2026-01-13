f85c539c22cea24aba93ae4130e7778784b3a93e
Gloomsfield <amory@acosta.boo>
2026-01-13 08:39:46 -0800

    preload assets

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,6 +3,12 @@ class Menu extends Phaser.Scene {
 		super('menu_scene');
 	}
 
+	preload() {
+		this.load.image('rocket', './assets/rocket.png');
+		this.load.image('spaceship', './assets/ship.png');
+		this.load.image('starfield', './assets/background.png');
+	}
+
 	create() {
 		this.add.text(20, 20, 'ROCKET PATROL MENU');
 
