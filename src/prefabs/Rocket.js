a577b109c8b639868b96ff3ac2684e55bdbe90ff
Gloomsfield <amory@acosta.boo>
2026-01-26 19:25:39 -0800

    tidy up rocket class

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,86 +2,71 @@ class Rocket extends Phaser.GameObjects.Sprite {
 	constructor(scene, r, theta) {
 		super(scene, r * Math.cos(theta), r * Math.sin(theta), 'rocket', 0);
 
-		this.r = r;
-		this.theta = theta;
-		this.goal_theta = theta;
-
-		this.offset = {
-			x: scene.cameras.main.width / 2,
-			y: scene.cameras.main.height / 2
-		};
-
 		scene.add.existing(this);
 		scene.physics.add.existing(this);
 
-		this.is_firing = false;
-		this.move_speed = 200;
-
-		this.move_direction = 0;
-
 		this.sfx_launch = scene.sound.add('sfx_rocket');
 
-		this.move_left_input = scene.input.keyboard.addKey(
-			Phaser.Input.Keyboard.KeyCodes.LEFT,
-			false,
-			true
-		);
-
-		this.move_right_input = scene.input.keyboard.addKey(
-			Phaser.Input.Keyboard.KeyCodes.RIGHT,
-			false,
-			true
-		);
-
-		this.fire_input = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-
-		this.fire_input.on('down', () => {
-			if(this.is_firing) {
-				return;
-			}
-			
-			this.is_firing = true;
-
-			this.sfx_launch.play();
-		});
-
-		this.reframe_input = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-
-		this.reframe_input.on('down', () => {
-			this.emit('reframe', 2.0 * Math.PI - this.theta);
-		});
-	}
-
-	get_cartesian() {
-		return {
-			x: this.r * Math.cos(this.theta) + this.offset.x,
-			y: this.r * Math.sin(this.theta) + this.offset.y,
+		this.viewport_center_offset = {
+			x: scene.cameras.main.width / 2,
+			y: scene.cameras.main.height / 2
 		};
+		
+		this.r = r;
+		this.theta = { target: theta, current: theta };
+
+		this.is_firing = false;
+
+		this.move_speed = 200;
+
+		let scene_keyboard = scene.input.keyboard;
+		this.move_left_input = scene_keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT, false, true);
+		this.move_right_input = scene_keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT, false, true);
+		this.fire_input = scene_keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+		this.reframe_input = scene_keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+
+		this.fire_input.on('down', this.fire, this);
+		this.reframe_input.on('down', this.reframe, this);
 	}
 
 	update(_, delta) {
-		if(!this.is_firing) {
-			this.move_direction = 0;
+		let move_direction = 0;
 
-			if(this.move_right_input.isDown) {
-				this.move_direction += 1;
-			}
+		move_direction += Number(this.move_right_input.isDown && !this.is_firing);
+		move_direction -= Number(this.move_left_input.isDown && !this.is_firing);
 
-			if(this.move_left_input.isDown) {
-				this.move_direction -= 1;
-			}
-
-			this.goal_theta += this.move_direction * this.move_speed * delta / 1000 / this.r;
-
-			this.theta = interpolate_angle(this.goal_theta, this.theta, this.move_speed * delta / 1000 / this.r);
-		} else {
-			this.r += this.move_speed * delta / 1000;
-		}
+		this.iterate_theta(delta, move_direction);
+		
+		this.r += Number(this.is_firing) * this.move_speed * delta / 1000;
 
 		this.x = this.get_cartesian().x;
 		this.y = this.get_cartesian().y;
 
-		this.rotation = this.theta + Math.PI / 2.0;
+		this.rotation = this.theta.current + Math.PI / 2.0;
+	}
+
+	fire() {
+		if(this.is_firing) { return; }
+
+		this.is_firing = true;
+	}
+
+	reframe() {
+		this.emit('reframe', 1.5 * Math.PI - this.theta.current);
+	}
+
+	get_cartesian() {
+		return {
+			x: this.r * Math.cos(this.theta.current) + this.viewport_center_offset.x,
+			y: this.r * Math.sin(this.theta.current) + this.viewport_center_offset.y,
+		};
+	}
+
+	iterate_theta(delta, direction) {
+		let angular_delta = this.move_speed * delta / 1000 / this.r;
+
+		this.theta.target += direction * angular_delta;
+		this.theta.current = interpolate_angle(this.theta.target, this.theta.current, angular_delta);
 	}
 
 	reset() {
